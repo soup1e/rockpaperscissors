96b48ad82dd70e1a7a7161eb91ad34d281fd8284
Sam adams <111674996+soup1e@users.noreply.github.com>
2022-09-13 09:29:32 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+/* Imports */
+
+/* State */
+
+/* Actions */
+function loadPage() {}
+
+/* Components */
+
+/* Component */
+// get DOM
+// display
+// event listeners
+
+/* Run page load code */
+loadPage();
