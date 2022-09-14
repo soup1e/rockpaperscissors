3913f4e0ee07aa2e8f93628b1c0fdba781af5bd7
Sam Adams <sam.adams7785@gmail.com>
2022-09-13 18:44:23 -0700

    initial text winlose

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ function randomShoots(array) {
     return item;
 }
 /* State */
-let gameState = 'choose'; // or results
 let total = '0';
 let wins = '0';
 let losses = '0';
@@ -19,16 +18,63 @@ let losses = '0';
 function loadPage() {
     displayScoreboard();
 }
+/* Component */
+const results = document.getElementById('results');
+const computerShootImage = document.getElementById('computer-shoot');
+const playerShootImage = document.getElementById('player-shoot');
 
 /* Components */
-function runGame(player) {
-    gameState = 'results';
-    const computerShoot = randomShoots(shoots);
-    console.log(computerShoot);
-    console.log(player);
-}
 
-/* Component */
+function runGame(player) {
+    const computerShoot = randomShoots(shoots);
+
+    console.log(computerShoot, 'computers throw');
+    console.log(player, 'players throw');
+
+    results.classList.remove('hidden'); // shows results
+
+    playerShootImage.src = `assets/${player}.jpg`; // CHANGES PLAYER IMG
+    computerShootImage.src = `assets/${computerShoot}.jpg`; // CHANGES COMPUTER IMG
+
+    const result = winner(player, computerShoot);
+
+    function winner(player, computerShoot) {
+        // PLAYER WINS
+        if (player === 'rock' && computerShoot === 'scissors') {
+            return 'player';
+        }
+        if (player === 'paper' && computerShoot === 'rock') {
+            return 'player';
+        }
+        if (player === 'scissors' && computerShoot === 'paper') {
+            return 'player';
+        }
+        // COMPUTER WINS
+        if (player === 'rock' && computerShoot === 'paper') {
+            return 'computer';
+        }
+        if (player === 'paper' && computerShoot === 'scissors') {
+            return 'computer';
+        }
+        if (player === 'scissors' && computerShoot === 'rock') {
+            return 'computer';
+        } else {
+            return 'tie';
+        }
+    }
+
+    const playerLose = document.getElementById('playerLose');
+    const playerWin = document.getElementById('playerWin');
+    const playerTie = document.getElementById('playerTie');
+    console.log(result);
+    if (result === 'player') {
+        playerWin.classList.remove('hidden');
+    } else if (result === 'computer') {
+        playerLose.classList.remove('hidden');
+    } else if (result === 'tie') {
+        playerTie.classList.remove('hidden');
+    }
+}
 
 // get DOM
 const rockShoot = document.getElementById('rock-shoot');
